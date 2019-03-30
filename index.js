24195ce5f42c818c6c69667abfa258eb131d22b6
march1997 <march1997@live.com>
2019-03-30 15:31:41 +0700

    Import fs-extra

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Load the SDK and UUID
 var AWS = require('aws-sdk')
+var fs = require('fs-extra')
 var credentials = new AWS.SharedIniFileCredentials({ profile: 'present' })
 AWS.config.credentials = credentials
 AWS.config.update({ region: 'ap-northeast-1' })
@@ -22,7 +23,7 @@ function uploadImage(uploadfile, studentID, courseID) {
     .promise()
 }
 
-var uploadfile = fs.readFileSync(req.file.path)
-uploadImage(uploadfile, '5831078221', '2110498').then(res => {
+var uploadfile = fs.readFileSync('C:/Users/March/Desktop/profile33.png')
+uploadImage(uploadfile, '5831071821', '2110498').then(res => {
   console.log('Successfully uploaded' + res.Key)
 })
