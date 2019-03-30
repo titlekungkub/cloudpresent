9806369d3795d87eaebece127b036b8db6539955
Arros <titlezakab@windowslive.com>
2019-03-30 15:54:42 +0700

    add rekognition

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var credentials = new AWS.SharedIniFileCredentials({ profile: 'present' })
 AWS.config.credentials = credentials
 AWS.config.update({ region: 'ap-northeast-1' })
 
+const rekognition = new AWS.Rekognition()
+
 // Create unique bucket name
 var bucketName = 'presentproject'
 
@@ -13,17 +15,75 @@ var s3 = new AWS.S3()
 
 // Handle promise fulfilled/rejected states
 
-function uploadImage(uploadfile, studentID, courseID) {
+function uploadRefImage(uploadfile, studentID, courseID) {
   return s3
     .upload({
       Bucket: bucketName,
       Body: uploadfile,
-      Key: '2018/2/' + courseID + '/' + studentID
+      Key: '2018/2/' + courseID + '/reference/' + studentID
+    })
+    .promise()
+}
+
+function uploadAttendanceImage(uploadfile, date, courseID) {
+  return s3
+    .upload({
+      Bucket: bucketName,
+      Body: uploadfile,
+      Key: '2018/2/' + courseID + '/attendance/' + date
     })
     .promise()
 }
 
 var uploadfile = fs.readFileSync('C:/Users/March/Desktop/profile33.png')
-uploadImage(uploadfile, '5831071821', '2110498').then(res => {
+uploadRefImage(uploadfile, '5831071821', '2110498').then(res => {
   console.log('Successfully uploaded' + res.Key)
+})
+
+var params = {
+  SimilarityThreshold: 90,
+  SourceImage: {
+    S3Object: {
+      Bucket: bucketName,
+      Name: '2018/2/2110498/reference/5831071821'
+    }
+  },
+  TargetImage: {
+    S3Object: {
+      Bucket: bucketName,
+      Name: '2018/2/2110498/attendance/5831071821'
+    }
+  }
+}
+rekognition.compareFaces(params, function(err, data) {
+  if (err) console.log(err, err.stack)
+  // an error occurred
+  else console.log(data) // successful response
+  /*
+     data = {
+      FaceMatches: [
+         {
+        Face: {
+         BoundingBox: {
+          Height: 0.33481481671333313, 
+          Left: 0.31888890266418457, 
+          Top: 0.4933333396911621, 
+          Width: 0.25
+         }, 
+         Confidence: 99.9991226196289
+        }, 
+        Similarity: 100
+       }
+      ], 
+      SourceImageFace: {
+       BoundingBox: {
+        Height: 0.33481481671333313, 
+        Left: 0.31888890266418457, 
+        Top: 0.4933333396911621, 
+        Width: 0.25
+       }, 
+       Confidence: 99.9991226196289
+      }
+     }
+     */
 })
