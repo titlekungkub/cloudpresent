f0457eb50616dec30925d91d9e31f5bd4c3b4c9f
Arros <titlezakab@windowslive.com>
2019-03-30 14:56:57 +0700

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+// Load the SDK and UUID
+var AWS = require('aws-sdk')
+var credentials = new AWS.SharedIniFileCredentials({ profile: 'present' })
+AWS.config.credentials = credentials
+AWS.config.update({ region: 'us-east-1' })
+
+// Create unique bucket name
+var bucketName = 'presentproject'
+
+// Create name for uploaded object key
+var keyName = 'hello_world.txt'
+
+// Create a promise on S3 service object
+var bucketPromise = new AWS.S3()
+
+// Handle promise fulfilled/rejected states
+bucketPromise
+  .then(function(data) {
+    // Create params for putObject call
+    var objectParams = {
+      Bucket: bucketName,
+      Key: keyName,
+      Body: 'Hello World!'
+    }
+    // Create object upload promise
+    var uploadPromise = new AWS.S3({ apiVersion: '2006-03-01' })
+      .putObject(objectParams)
+      .promise()
+    uploadPromise.then(function(data) {
+      console.log('Successfully uploaded data to ' + bucketName + '/' + keyName)
+    })
+  })
+  .catch(function(err) {
+    console.error(err, err.stack)
+  })
