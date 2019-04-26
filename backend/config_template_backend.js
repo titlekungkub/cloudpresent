db92d3d721524a7bdbacda3b29d36a914f3be1f8
titlekungkub <titlezakab@windowslive.com>
2019-04-26 23:09:37 +0700

    Create config_template_backend.js

---
+++ backend/config_template_backend.js
@@ -0,0 +1,4 @@
+module.exports.region = <AWS S3 region>
+module.exports.bucket = <AWS S3 Bucket name>
+module.exports.keyPath = <Object Key in AWS S3 Bucket>
+module.exports.imgDir = <Your reference image folder name>
